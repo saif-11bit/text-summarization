8d0be65a98f51ab3011a744542fe67aba7e75c42
saif-11bit <saifmdco@gmail.com>
2024-11-09 15:56:47 +0530

    requirements installation done

---
+++ setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+
+__version__ = "0.0.0"
+
+REPO_NAME = "text-summarization"
+AUTHOR_USER_NAME = "saif-11bit"
+SRC_REPO = "textSummarizer"
+AUTHOR_EMAIL = "saifmdco@gmail.com"
+
+
+setuptools.setup(
+    name=REPO_NAME,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="python NLP package for text summarization",
+    long_description=long_description,
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls = {
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
+)
